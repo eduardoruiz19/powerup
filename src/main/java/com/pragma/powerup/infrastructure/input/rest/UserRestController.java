617db325e0dd2d9bb2d186080e40e1d98bc8d73d
Eduardo Ruiz <eduardoruiz19@hotmail.com>
2023-02-16 20:40:31 -0500

    USUARIOS SOLUCIONADO con excepciones

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/UserRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/UserRestController.java
@@ -14,11 +14,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -28,15 +31,70 @@ public class UserRestController {
     private final IUserHandler userHandler;
 
 
+    @Operation(summary = "Add a new User Owner")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User Owner created", content = @Content),
+            @ApiResponse(responseCode = "409", description = "User Owner already exists", content = @Content)
+    })
+    @PostMapping("/owner")
+    public ResponseEntity<?> saveOwner(@RequestBody UserRequestDto userRequestDto) {
+        System.out.println("llega a saveOwner");
+        userRequestDto.setRol("ADMIN");
+        Map<String,Object > response = new HashMap<>();
+        try{
+            userHandler.saveUser(userRequestDto);
+        }catch (DataAccessException e){
+            response.put("mensaje","Data Insertion Error");
+            response.put("error",e.getMessage());
+            //response.put("error",e.getMostSpecificCause());
+            return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+        return new ResponseEntity<UserRequestDto>(HttpStatus.CREATED);
+    }
+
+
+    @Operation(summary = "Add a new User Client")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User Client created", content = @Content),
+            @ApiResponse(responseCode = "409", description = "User Client already exists", content = @Content)
+    })
+    @PostMapping("/client")
+    public ResponseEntity<?> saveClient(@RequestBody UserRequestDto userRequestDto) {
+        //System.out.println("llega a saveOwner");
+        userRequestDto.setRol("CLIENT");
+        Map<String,Object > response = new HashMap<>();
+        try{
+            userHandler.saveUser(userRequestDto);
+        }catch (DataAccessException e){
+            response.put("mensaje","Data Insertion Error");
+            response.put("error",e.getMessage());
+            //response.put("error",e.getMostSpecificCause());
+            return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+        return new ResponseEntity<UserRequestDto>(HttpStatus.CREATED);
+    }
+
+
     @Operation(summary = "Add a new User")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "User created", content = @Content),
             @ApiResponse(responseCode = "409", description = "User already exists", content = @Content)
     })
     @PostMapping("/")
-    public ResponseEntity<UserRequestDto> saveUser(@RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<?> saveUser(@RequestBody UserRequestDto userRequestDto) {
         System.out.println("llega a saveUser");
-        userHandler.saveUser(userRequestDto);
+        Map<String,Object > response = new HashMap<>();
+        try{
+            userHandler.saveUser(userRequestDto);
+        }catch (DataAccessException e){
+            response.put("mensaje","Data Insertion Error");
+            response.put("error",e.getMessage());
+            return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
         return new ResponseEntity<UserRequestDto>(HttpStatus.CREATED);
     }
